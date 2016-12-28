0f8dffbe388541bb559f7439833469b5ab6b7b01
Michelle Sri <michelle.sri@gmail.com>
2016-12-27 16:34:43 -0800

    added token and user service

--- app/src/app.js
+++ app/src/app.js
@@ -5,17 +5,45 @@ import services from './services';
 import uiRouter from 'angular-ui-router';
 import routes from './routes';
 
+//plugin for 'default' routes
+import defaultRoute from 'angular-ui-router-default';
+
+//need this for $stateChanged events
+import 'angular-ui-router/release/stateEvents';
+
+import dialog from 'ng-dialog';
+import 'ng-dialog/css/ngDialog.css';
+import 'ng-dialog/css/ngDialog-theme-default.css';
+
+//route, http config and auth setup
+import http from './http';
+import auth from './auth';
+
+
 
 const app = angular.module('myApp', [
     components,
     services,
-    uiRouter
+    uiRouter,
+    angular.module('ui.router.state.events').name,
+    defaultRoute,
+    dialog
 ]);
 
-app.config(routes);
+app.filter('titleCase', () => {
+  return function titleCaseFilter(input){
+    if(!input) return '';
+    return input[0].toUpperCase() + input.slice(1);
+  };
+});
 
-const dev = 'https://pet-store-401.herokuapp.com/api/unauth';
+
+const dev = 'https://pet-store-401.herokuapp.com/api/';
 
 app.value('apiUrl', dev);
+
+app.config(http);
+app.config(routes);
+app.run(auth);
 
 export default app;
